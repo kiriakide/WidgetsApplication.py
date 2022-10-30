e1d76e7e6562d4899ee2f99ee892c69edd193e75
kiriakide <kiriakigew20@gmail.com>
2022-10-30 22:30:21 +0200

    hi

--- pages/hey.py
+++ pages/hey.py
@@ -6,6 +6,6 @@ r = requests.get(url)
 data = r.json()
 
 name = data['Meta Data']['2. Symbol']
-open = data['Time Series (Daily)']['2022-10-28']['1.open']
+openj = data['Time Series (Daily)']['2022-10-28']['1.open']
 st.write(name)
-st.write(open)
+st.write(openj)
