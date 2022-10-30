c1e11d215e27b29f96e830fefdf1416904bb400c
kiriakide <kiriakigew20@gmail.com>
2022-10-30 22:52:10 +0200

    hi

--- pages/hey.py
+++ pages/hey.py
@@ -1,10 +1,12 @@
-import requests
 import streamlit as st
+import pandas as pd
+import requests
 
-url= 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo'
 r = requests.get(url)
 data = r.json()
-
+#st.json(data)
 name = data['Meta Data']['2. Symbol']
 open = data['Time Series (Daily)']['2022-10-28']['1. open']
 st.write(name)
