5e507fed9def56c52daeda77d3f33df4a60e4122
kiriakide <kiriakigew20@gmail.com>
2022-10-30 22:18:37 +0200

    hi

--- pages/hey.py
+++ pages/hey.py
@@ -1,6 +1,5 @@
-import streamlit as st
-import pandas as pd
 import requests
+import streamlit as st
 
 url= 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
 r = requests.get(url)
