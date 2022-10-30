048894e967ccbc50c3f4f8626186394f2f1cce6b
kiriakide <kiriakigew20@gmail.com>
2022-10-30 21:26:01 +0200

    hi

--- pages/hey.py
+++ pages/hey.py
@@ -8,3 +8,10 @@ with st.container():
     st.subheader("hey")
 
 st.sidebar.success("select a page above")
+
+import urllib.request, json
+
+resp = urllib.request.urlopen('https://query2.finance.yahoo.com/v10/finance/quoteSummary/tsla?modules=price')
+data = json.loads(resp.read())
+price = data['quoteSummary']['result'][0]['price']['regularMarketPrice']['raw']
+st.print(price)
